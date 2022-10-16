bd2e561f8a2c47f67d1d5d942285eba4ed978904
Emre <emre.tutuncu@hotmail.com.tr>
2022-10-17 02:12:10 +0300

    Added redirect to main page (with main layout)

--- IoMI/Client/Utils/CustomAuthStateProvider.cs
+++ IoMI/Client/Utils/CustomAuthStateProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
@@ -7,35 +8,48 @@ namespace IoMI.Client.Utils
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
         private readonly IJSRuntime _js;
+        private readonly NavigationManager _navigationManager;
 
-        public CustomAuthStateProvider(IJSRuntime js)
+        public CustomAuthStateProvider(IJSRuntime js, NavigationManager navigationManager)
         {
             _js = js;
+            _navigationManager = navigationManager;
         }
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             string token = await _js.InvokeAsync<string>("localStorage.getItem", "accessToken") ?? string.Empty;
-            JwtSecurityTokenHandler handler = new();
-
-            ClaimsIdentity claims = new();
             if (!string.IsNullOrEmpty(token.Trim()))
+            {
+                AuthenticationState state;
+                ClaimsIdentity claims = new();
+                JwtSecurityTokenHandler handler = new();
                 try
                 {
                     JwtSecurityToken securityToken = handler.ReadJwtToken(token);
                     if (securityToken.ValidTo > DateTime.UtcNow)
                         claims = new(securityToken.Claims, "jwt");
                     else
+                    {
                         await _js.InvokeVoidAsync("localStorage.removeItem", "accessToken");
+                        _navigationManager.NavigateTo("/user/login");
+                    }
                 }
-                catch (Exception) { }
-
-            ClaimsPrincipal user = new(claims);
-            AuthenticationState state = new(user);
-
-            NotifyAuthenticationStateChanged(Task.FromResult(state));
-
-            return state;
+                catch (Exception)
+                {
+                    await _js.InvokeVoidAsync("localStorage.removeItem", "accessToken");
+                    _navigationManager.NavigateTo("/user/login");
+                }
+                ClaimsPrincipal user = new(claims);
+                state = new(user);
+                NotifyAuthenticationStateChanged(Task.FromResult(state));
+                return state;
+            }
+            else
+            {
+                _navigationManager.NavigateTo("/user/login");
+            }
+            return new(new());
         }
 
         //public static IEnumerable<Claim> GetClaimsFromJwt(string jwtToken)
